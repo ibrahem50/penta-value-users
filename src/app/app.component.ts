c5908b2f0d1223e5e9cf8a02e23111c86fa9d6c6
Rawan Helmy <rawanhelmy98@gmail.com>
2023-05-26 00:18:57 +0300

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,24 +10,45 @@ import { AngularFirestore } from "@angular/fire/firestore";
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
-  private basePath = "/uploads";
-
+  filelist: any[] = [];
+  lastImage;
+  lastItem;
+  type = "";
   constructor(
     private db: AngularFireDatabase,
     private storage: AngularFireStorage
   ) {}
   ngOnInit(): void {
-    // this.db
-    //   .list(this.basePath, (ref) => ref.limitToLast(1000))
-    //   .snapshotChanges()
-    //   .subscribe((res) => {
-    //     console.log(res);
-    //   });
-    this.storage
-      .ref("/")
-      .listAll()
-      .subscribe((res) => {
-        console.log(res);
-      });
+    const ref = this.storage.ref("/");
+    let myurlsubscription = ref.listAll().subscribe((data) => {
+      for (let i = 0; i < data.items.length; i++) {
+        let newref = this.storage.ref(data.items[i].name);
+        newref.getMetadata().subscribe((res) => {
+          this.filelist.push(res);
+          if (i == data.items.length - 1) {
+            // this.filelist.sort(
+            //   (a: any, b: any) =>
+            //     new Date(a.timeCreated).getTime() -
+            //     new Date(b.timeCreated).getTime()
+            // );
+            this.setLast(res);
+          }
+        });
+      }
+    });
+  }
+  downloadLastImage(data) {
+    let newref = this.storage.ref(data.name);
+    newref.getDownloadURL().subscribe((res) => {
+      this.lastImage = res;
+      window.open(res, "_blank");
+    });
+  }
+  setLast(data) {
+    let newref = this.storage.ref(data.name);
+    newref.getDownloadURL().subscribe((res) => {
+      // console.log(res);
+      this.lastImage = res;
+    });
   }
 }
